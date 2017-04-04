96a21557ff261e7c5aecf3b9f9afadf420a71688
pjeong <pjeong@riotgames.com>
2017-04-04 22:25:43 +0900

    Add HMR to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	entry:'./app/index.js',
+	entry: [
+		'webpack-dev-server/client?http://localhost:3008',
+		'webpack/hot/only-dev-server',
+		'./app/index.js'
+	],
+
 	output:{
 		filename: 'bundle.js',
 		path:path.resolve(__dirname,'dist')
+	},
+
+	plugins:[
+		new webpack.HotModuleReplacementPlugin()
+	],
+
+	devServer: {
+		hot: true,
+		contentBase: './app'
 	}
 }
